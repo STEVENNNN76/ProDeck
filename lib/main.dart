573b562cdca9f8f9018b15adbab60944c62a4218
Steven Luke <93815094+STEVENNNN76@users.noreply.github.com>
2023-07-07 14:57:08 +0530

    Working Notifcation

--- lib/main.dart
+++ lib/main.dart
@@ -39,10 +39,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      //home: SplashScreen(),
-      home: NotificationButton(
-        title: 'hi',
-      ),
+      home: SplashScreen(),
       //home: CalenderView(),
       //home: MyHomePage(),
       //home: WorkPage(),
